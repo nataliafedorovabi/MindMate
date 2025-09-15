5fa03dcdddef66615e59b77cefeb284277dbcadf
nataliafedorovabi <140884272+nataliafedorovabi@users.noreply.github.com>
2025-09-15 11:36:43 +0300

    Create __init__.py

---
+++ app/routers/__init__.py
@@ -0,0 +1,6 @@
+from .start import router as start
+from .library import router as library
+from .journal import router as journal
+from .checklists import router as checklists
+
+__all__ = ["start", "library", "journal", "checklists"]
